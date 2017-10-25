d4b82c471a4f0efd4e193111e876aba403b242c7
Yasser Makram <yasser@barm.ag>
2017-10-25 16:49:31 +0200

    implement only choice examines peers and  find boxes with a single digit is the only choice

--- solution.py
+++ solution.py
@@ -78,7 +78,13 @@ def eliminate(values):
     return values
 
 def only_choice(values):
-    pass
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    print(values)
+    return values
 
 def reduce_puzzle(values):
     pass
