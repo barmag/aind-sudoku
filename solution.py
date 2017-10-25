c533c2dc3ebd606b3b1b5c660b3021bf06d90cae
Yasser Makram <yasser@barm.ag>
2017-10-25 17:53:02 +0200

    implement search for sudoku solution using depth first searh, try multiple eliminate, only choice iterations to find a solution

--- solution.py
+++ solution.py
@@ -100,7 +100,20 @@ def reduce_puzzle(values):
     return values
 
 def search(values):
-    pass
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and 
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
